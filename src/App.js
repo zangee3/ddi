fed138cc610d7b889eef2384e37721b6ebe1f33f
Sanjeev Kumar <sanjeev@gpcoders.com>
2021-01-13 18:36:51 +0530

    implement design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import DNSOptions from "./DNSOptions";
-import CNameOptions from "./CNameOptions";
-import TxtOptions from "./TxtOptions";
-import MXOptions from "./MXOptions";
-
+import DnsSetting from './DnsSetting';
+import Login from './Login';
 function App() {
+  //let showNav=false
   return (
-    <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-3">Navigation Items</div>
-          <div className="col-md-9">
-            <DNSOptions/>
-            <CNameOptions/>
-            <TxtOptions/>
-            <MXOptions/>
-          </div>
-        </div>
+    <Router>
+      <div className='App'>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Login</Link>
+            </li>
+            <li>
+              <Link to='/dns/setting'>DNS Setting</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path='/dns/setting'>
+            <DnsSetting />
+          </Route>
+          <Route path='/'>
+            <Login />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
