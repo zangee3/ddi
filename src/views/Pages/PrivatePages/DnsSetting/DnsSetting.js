3e29045ac9d11e967c1d7d7a25eb4ced95d630ee
sanjeev <sanjeev@gpcoders.com>
2021-01-18 17:53:02 +0530

    Css Changes

--- src/views/Pages/PrivatePages/DnsSetting/DnsSetting.js
+++ src/views/Pages/PrivatePages/DnsSetting/DnsSetting.js
@@ -28,35 +28,43 @@ const DnsSetting = () => {
       <Tabs defaultActiveKey="DNS" transition={false} id="noanim-tab-example">
   <Tab eventKey="DNS" title="DNS">
   <div className="row">
-    
-    <div
-      className="col-md-3 navigation "
-    >
-      <Nav
-        activeKey="/home"
-        // onSelect={selectedKey => alert(`selected ${selectedKey}`)}
-      >
+        <Tab.Container id="left-tabs-example-3" defaultActiveKey="A Records">
+ 
+        <div className="col-md-3 navigation">
+      <Nav variant="pills" className="flex-column">
         <Nav.Item>
-          <Nav.Link href="/home">A Records</Nav.Link>
+          <Nav.Link eventKey="A Records">A Records</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-1">CNAME Records</Nav.Link>
+          <Nav.Link eventKey="CNAME Records">CNAME Records</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-2">TXT Records</Nav.Link>
+          <Nav.Link eventKey="TXT Records">TXT Records</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-3">MX Records</Nav.Link>
+          <Nav.Link eventKey="MX Records">MX Records</Nav.Link>
         </Nav.Item>
       </Nav>
     </div>
     <div className="col-md-9 form-sec">
-      <DNSOptions />
-      <CNameOptions />
-      <TxtOptions />
-      <MXOptions />
-    </div>
-  </div>
+      <Tab.Content>
+        <Tab.Pane eventKey="A Records">
+        <DNSOptions />
+        </Tab.Pane>
+        <Tab.Pane eventKey="CNAME Records">
+        <CNameOptions />
+        </Tab.Pane>
+        <Tab.Pane eventKey="TXT Records">
+        <TxtOptions />
+        </Tab.Pane>
+        <Tab.Pane eventKey="MX Records">
+        <MXOptions />
+        </Tab.Pane>
+      </Tab.Content>
+   </div>
+  
+</Tab.Container>
+</div>
 
   </Tab>
   <Tab eventKey="Firewall" title="Firewall">
@@ -68,30 +76,31 @@ const DnsSetting = () => {
   </Tab>
   <Tab eventKey="Network" title="Network">
   <div className="row">
-    
-    <div
-      className="col-md-3 navigation"
-    >
-      <Nav
-        activeKey="/home"
-        onSelect={selectedKey => alert(`selected ${selectedKey}`)}
-      >
+        <Tab.Container id="left-tabs-example-2" defaultActiveKey="DHCP">
+ 
+        <div className="col-md-3 navigation">
+      <Nav variant="pills" className="flex-column">
         <Nav.Item>
-          <Nav.Link href="/home">DHCP</Nav.Link>
+          <Nav.Link eventKey="DHCP">DHCP </Nav.Link>
         </Nav.Item>
         
       </Nav>
     </div>
     <div className="col-md-9 form-sec">
-    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p>
-    </div>
-  </div>
+      <Tab.Content>
+       <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</p>
+      </Tab.Content>
+   </div>
+  
+</Tab.Container>
+</div>
   </Tab>
 </Tabs>
         </div>
 
-      
-    </div>
+
+</div>
+   
   );
 };
 
