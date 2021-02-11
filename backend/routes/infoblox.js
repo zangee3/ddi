8a921f19c2e270859510b5bc5817c7a6560fd61c
Pardip Bhatti <pardipbhatti8791@gmail.com>
2021-02-11 14:27:41 +0530

    changing axios to callback

--- backend/routes/infoblox.js
+++ backend/routes/infoblox.js
@@ -1,40 +1,32 @@
-var express = require("express");
-var router = express.Router();
-const axios = require("axios");
-const https = require('https')
+const express = require("express");
+const router = express.Router();
+const request = require('request');
+
+
 
 router.post("/", function (req, res, next) {
-  console.log(JSON.stringify(req.body));
-  axios
-    .post(
-      "https://10.92.18.84/wapi/v2.9/record:host?_return_fields=name,ipv4addrs&_return_as_object=1",
-      JSON.stringify(req.body),
-      {
-        headers: {
-          "content-type": "application/json",
-        },
-          httpsAgent: new https.Agent({
-              rejectUnauthorized: false
-          }),
-        params: {
-          rejectUnauthorized: false,
-          requestCert: true,
-          agent: false,
-        },
+    const options = {
+        url: 'https://10.92.18.84/wapi/v2.9/record:host?_return_fields=name,ipv4addrs&_return_as_object=1',
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: req.body,
+        rejectUnauthorized: false,
+        requestCert: true,
+        agent: false,
         auth: {
-          user: "ashams",
-          pass: "M0arAutomation!",
-        },
-      }
-    )
-    .then((response) => {
-      console.log("response: ", response);
-      return res.status(200).json(response);
-    })
-    .catch((err) => {
-      console.log("err: ", err);
-      res.status(400).json(err);
-    });
+            'user': 'ashams',
+            'pass': 'M0arAutomation!'
+        }
+    };
+    request(options, callback);
 });
+
+function callback(error, response, body) {
+    if (!error && response.statusCode >= 200) {
+        console.log(body);
+    } else {
+        console.log("error-----",response.statusCode,body);
+    }
+}
 
 module.exports = router;
