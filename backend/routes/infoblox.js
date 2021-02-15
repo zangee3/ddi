229719f0c85c2de120d0891c17e24b44dadc29cc
Pardip Bhatti <pardipbhatti8791@gmail.com>
2021-02-15 18:35:54 +0530

    working dns mapping

--- backend/routes/infoblox.js
+++ backend/routes/infoblox.js
@@ -4,6 +4,16 @@ const request = require('request');
 const fs = require('fs');
 
 router.post("/", function (req, res, next) {
+
+    fs.appendFileSync('log.txt', req.body );
+    fs.readFile("records.txt", 'utf8', function (err, data) {
+        if (err) throw err;
+        res.status(200).send(data)
+    })
+
+    fs.appendFileSync('records.json', JSON.stringify(req.body));
+    return res.send(req.body);
+
     const options = {
         url: 'https://10.92.18.84/wapi/v2.9/record:host?_return_fields=name,ipv4addrs&_return_as_object=1',
         method: 'POST',
@@ -18,20 +28,28 @@ router.post("/", function (req, res, next) {
         }
     };
     return request(options, function (error, response, body) {
+
         let fileData = '\n\nRequest Body: '+options.body+'\n';
         if (!error && response.statusCode >= 200) {
             console.log(body);
             fileData += 'Infoblox Response:' + body + '\n\n';
             fs.appendFileSync('log.txt', fileData, "utf8");
-            fs.appendFileSync('records.txt', options.body+'\n\n', "utf8");
-            // console.log("File written successfully\n"); 
-            // console.log("The written has the following contents:"); 
-            // console.log(fs.readFileSync("log.txt", "utf8")); 
+            fs.appendFileSync('records.json', options.body, "utf8");
+            // console.log("File written successfully\n");
+            // console.log("The written has the following contents:");
+            // console.log(fs.readFileSync("log.txt", "utf8"));
             return res.send(body);
         } else {
             console.log("error-----",response.statusCode,body);
         }
     });
 });
+
+router.get("/dns", function (req, res, next) {
+    fs.readFile("records.txt", 'utf8', function (err, data) {
+        if (err) throw err;
+        res.status(200).send(data)
+    })
+})
 
 module.exports = router;
