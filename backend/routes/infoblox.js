628496ce4ec5e87113b4be24120314b00291aa18
Pardip Bhatti <pardipbhatti8791@gmail.com>
2021-02-16 16:30:25 +0530

    working on delete

--- backend/routes/infoblox.js
+++ backend/routes/infoblox.js
@@ -55,4 +55,34 @@ router.get("/dns", function (req, res, next) {
     });
 })
 
+router.post("/delete", function (req, res, next) {
+    const recordName = req.body.name
+    const recordId = req.body.id
+    var dataString = '[{"method": "STATE:ASSIGN","data":{"host_name": ' + recordName + '}},{"method":"GET","object": "record:host","data":{"name":"##STATE:host_name:##"},"assign_state": {"host_ref": "_ref"},"enable_substitution": true,"discard": true},{"method": "DELETE", "object": "##STATE:host_ref:##","enable_substitution": true,"discard": true},{"method":"STATE:DISPLAY"}]';
+    const options = {
+        url: 'https://10.92.18.84/wapi/v2.9/request',
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: dataString,
+        rejectUnauthorized: false,
+        requestCert: true,
+        agent: false,
+        auth: {
+            'user': 'codetesting',
+            'pass': 'D0ntGoB00m!'
+        }
+    };
+
+    return request(options, function (error, response, body) {
+        if (!error && response.statusCode >= 200) {
+            res.status(200).json({
+                success: true,
+                message: "Record deleted successfully"
+            })
+        } else {
+            console.log("error-----",response.statusCode,body);
+        }
+    });
+})
+
 module.exports = router;
