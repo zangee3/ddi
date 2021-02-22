532571e44c140f8b7c56955010e4dd1de0f6c5fb
pardipbhatti8791 <pardipbhatti8791@gmail.com>
2021-02-22 14:42:23 +0530

    design ui fixed

--- frontend/src/views/Components/MXOptions.js
+++ frontend/src/views/Components/MXOptions.js
@@ -1,11 +1,13 @@
-import React from "react";
-import RenderMXFields from "./RenderMXFields";
+import React from 'react';
+import RenderMXFields from './RenderMXFields';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 class MXOptions extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 1
+      value: 1,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -18,7 +20,7 @@ class MXOptions extends React.Component {
 
   handleSubmit(event) {
     const { value } = this.state;
-    alert("Your Selected Number of DNS Entries are: " + value);
+    alert('Your Selected Number of DNS Entries are: ' + value);
     event.preventDefault();
   }
 
@@ -26,28 +28,110 @@ class MXOptions extends React.Component {
     const { value } = this.state;
     console.log(value);
     return (
-      <div className="m-bottom">
+      <div className='m-bottom'>
         <form onSubmit={this.handleSubmit}>
           <h6>MX Records</h6>
-          <div className="form-group">
-            <label htmlFor="exampleFormControlSelect2">
-              Number of TXT Records
-            </label>
-            <select
-              value={value}
-              onChange={this.handleChange}
-              className="form-control"
-              id="exampleFormControlSelect2"
-            >
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
+          <div className='row d-flex'>
+            <div className='col-md-6 '>
+              <div className='form-group '>
+                <label
+                  htmlFor='exampleFormControlSelect2'
+                  className='d-block mb-2 font-weight-bold'
+                >
+                  Number of TXT Records
+                </label>
+                <select
+                  value={value}
+                  onChange={this.handleChange}
+                  className='form-control'
+                  id='exampleFormControlSelect2'
+                >
+                  <option value='1'>1</option>
+                  <option value='2'>2</option>
+                  <option value='3'>3</option>
+                  <option value='4'>4</option>
+                  <option value='5'>5</option>
+                </select>
+              </div>
+            </div>
           </div>
           {value ? <RenderMXFields value={value} /> : null}
           {/*<button type="submit" className="btn btn-primary">Submit</button>*/}
+          <table className='table table-bordered bg-white mt-5'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>IP</th>
+                <th class='text-center'>Actions</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>ahmed.test2.com</td>
+                <td>1.1.1.2 / 3.4.5.6</td>
+                <td>
+                  <div className='d-flex justify-content-around'>
+                    <span style={{ cursor: 'pointer' }}>
+                      <FontAwesomeIcon
+                        icon={faTimesCircle}
+                        className={'text-danger'}
+                      />
+                    </span>
+
+                    <span style={{ cursor: 'pointer' }}>
+                      <FontAwesomeIcon
+                        icon={faEdit}
+                        className={'text-primary'}
+                      />
+                    </span>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>ahmed.test2.com</td>
+                <td>1.1.1.2 / 3.4.5.6</td>
+                <td>
+                  <div className='d-flex justify-content-around'>
+                    <span style={{ cursor: 'pointer' }}>
+                      <FontAwesomeIcon
+                        icon={faTimesCircle}
+                        className={'text-danger'}
+                      />
+                    </span>
+
+                    <span style={{ cursor: 'pointer' }}>
+                      <FontAwesomeIcon
+                        icon={faEdit}
+                        className={'text-primary'}
+                      />
+                    </span>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>ahmed.test2.com</td>
+                <td>1.1.1.2 / 3.4.5.6</td>
+                <td>
+                  <div className='d-flex justify-content-around'>
+                    <span style={{ cursor: 'pointer' }}>
+                      <FontAwesomeIcon
+                        icon={faTimesCircle}
+                        className={'text-danger'}
+                      />
+                    </span>
+
+                    <span style={{ cursor: 'pointer' }}>
+                      <FontAwesomeIcon
+                        icon={faEdit}
+                        className={'text-primary'}
+                      />
+                    </span>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </form>
       </div>
     );
--- frontend/src/views/Components/TxtOptions.js
+++ frontend/src/views/Components/TxtOptions.js
@@ -1,55 +1,141 @@
 import React from 'react';
 import RenderTxtFields from './RenderTxtFields';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 class TxtOptions extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: 0,
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+    };
 
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-	handleChange(event) {
-		this.setState({ value: event.target.value });
-	}
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-	handleSubmit(event) {
-		const { value } = this.state;
-		alert('Your Selected Number of DNS Entries are: ' + value);
-		event.preventDefault();
-	}
+  handleSubmit(event) {
+    const { value } = this.state;
+    alert('Your Selected Number of DNS Entries are: ' + value);
+    event.preventDefault();
+  }
 
-	render() {
-		const { value } = this.state;
-		return (
-			<div className="m-bottom">
-				<form onSubmit={this.handleSubmit}>
-					<h6>TXT Records</h6>
-					<div className="form-group">
-						<label htmlFor="exampleFormControlSelect2">Select quantity of TXT Records</label>
-						<select
-							value={value}
-							onChange={this.handleChange}
-							className="form-control"
-							id="exampleFormControlSelect2"
-						>
-							<option value="0">0</option>
-							<option value="1">1</option>
-							<option value="2">2</option>
-							<option value="3">3</option>
-							<option value="4">4</option>
-							<option value="5">5</option>
-						</select>
-					</div>
-					{value ? <RenderTxtFields value={value} /> : null}
-					{/*<button type="submit" className="btn btn-primary">Submit</button>*/}
-				</form>
-			</div>
-		);
-	}
+  render() {
+    const { value } = this.state;
+    return (
+      <div className='m-bottom'>
+        <form onSubmit={this.handleSubmit}>
+          <h6>TXT Records</h6>
+          <div className='row d-flex'>
+            <div className='col-md-6 '>
+              <div className='form-group '>
+                <label
+                  htmlFor='exampleFormControlSelect2'
+                  className='d-block mb-2 font-weight-bold'
+                >
+                  Select quantity of TXT Records
+                </label>
+                <select
+                  value={value}
+                  onChange={this.handleChange}
+                  className='form-control'
+                  id='exampleFormControlSelect2'
+                >
+                  <option value='0'>0</option>
+                  <option value='1'>1</option>
+                  <option value='2'>2</option>
+                  <option value='3'>3</option>
+                  <option value='4'>4</option>
+                  <option value='5'>5</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          {value ? <RenderTxtFields value={value} /> : null}
+          {/*<button type="submit" className="btn btn-primary">Submit</button>*/}
+          <table className='table table-bordered bg-white mt-5'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>IP</th>
+                <th class='text-center'>Actions</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>ahmed.test2.com</td>
+                <td>1.1.1.2 / 3.4.5.6</td>
+                <td>
+                  <div className='d-flex justify-content-around'>
+                    <span style={{ cursor: 'pointer' }}>
+                      <FontAwesomeIcon
+                        icon={faTimesCircle}
+                        className={'text-danger'}
+                      />
+                    </span>
+
+                    <span style={{ cursor: 'pointer' }}>
+                      <FontAwesomeIcon
+                        icon={faEdit}
+                        className={'text-primary'}
+                      />
+                    </span>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>ahmed.test2.com</td>
+                <td>1.1.1.2 / 3.4.5.6</td>
+                <td>
+                  <div className='d-flex justify-content-around'>
+                    <span style={{ cursor: 'pointer' }}>
+                      <FontAwesomeIcon
+                        icon={faTimesCircle}
+                        className={'text-danger'}
+                      />
+                    </span>
+
+                    <span style={{ cursor: 'pointer' }}>
+                      <FontAwesomeIcon
+                        icon={faEdit}
+                        className={'text-primary'}
+                      />
+                    </span>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>ahmed.test2.com</td>
+                <td>1.1.1.2 / 3.4.5.6</td>
+                <td>
+                  <div className='d-flex justify-content-around'>
+                    <span style={{ cursor: 'pointer' }}>
+                      <FontAwesomeIcon
+                        icon={faTimesCircle}
+                        className={'text-danger'}
+                      />
+                    </span>
+
+                    <span style={{ cursor: 'pointer' }}>
+                      <FontAwesomeIcon
+                        icon={faEdit}
+                        className={'text-primary'}
+                      />
+                    </span>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default TxtOptions;
