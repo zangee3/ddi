47c632ef131365096436679bc8d4a576523fce0e
Ahmed Shams <ahmed.x.shams.-nd@disney.com>
2021-03-10 16:59:29 +0200

    implemented passport-saml

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,36 +1,63 @@
-const saml = require('samlify');
-var express = require('express');
-var router = express.Router();
-const fs = require('fs')
+var http = require('http');
+var fs = require('fs');
+var express = require("express");
+var dotenv = require('dotenv').config({ path: '.env' });
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
+var saml = require('passport-saml');
 
-const ServiceProvider = saml.ServiceProvider;
-const IdentityProvider = saml.IdentityProvider;
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+ 
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
-// Configure your endpoint for IdP-initiated / SP-initiated SSO
-const sp = ServiceProvider({
-    metadata: fs.readFileSync(__dirname + '/../xml/metadata_sp.xml')
+var samlStrategy = new saml.Strategy({
+  // URL that goes from the Identity Provider -> Service Provider
+  callbackUrl: process.env.CALLBACK_URL,
+  // URL that goes from the Service Provider -> Identity Provider
+  entryPoint: process.env.ENTRY_POINT,
+  // Usually specified as `/shibboleth` from site root
+  issuer: process.env.ISSUER,
+  identifierFormat: null,
+  // Service Provider private key
+  decryptionPvk: fs.readFileSync(__dirname + '/key.pem', 'utf8'),
+  // Service Provider Certificate
+  privateCert: fs.readFileSync(__dirname + '/key.pem', 'utf8'),
+  // Identity Provider's public key
+  cert: fs.readFileSync(__dirname + '/idp_cert.pem', 'utf8'),
+  validateInResponseTo: false,
+  disableRequestedAuthnContext: true
+}, function(profile, done) {
+  return done(null, profile); 
 });
-const idp = IdentityProvider({
-    metadata: fs.readFileSync(__dirname + '/../xml/onelogin_metadata_487043.xml')
-});
+
+passport.use(samlStrategy);
 
 // Release the metadata publicly
-router.get('/metadata', (req, res) => res.header('Content-Type','text/xml').send(sp.getMetadata()));
+router.get('/metadata', (req, res) => {
+    res.type('application/xml');
+    res.status(200).send(samlStrategy.generateServiceProviderMetadata(fs.readFileSync(__dirname + '/cert.pem', 'utf8')));
+});
 
 // Access URL for implementing SP-init SSO
-router.get('/spinitsso-redirect', (req, res) => {
-    const { id, context } = sp.createLoginRequest(idp, 'redirect');
-    return res.redirect(context);
-});
+// router.get('/spinitsso-redirect', (req, res) => {
+//     const { id, context } = sp.createLoginRequest(idp, 'redirect');
+//     return res.redirect(context);
+// });
 
 // If your application only supports IdP-initiated SSO, just make this route is enough
 // This is the assertion service url where SAML Response is sent to
 router.post('/acs', (req, res) => {
-    sp.parseLoginResponse(idp, 'post', req)
-        .then(parseResult => {
-            // ...
-            console.log("parseResult: ", parseResult)
-        })
-        .catch(console.error);
+    // sp.parseLoginResponse(idp, 'post', req)
+    //     .then(parseResult => {
+    //         // ...
+    //         console.log("parseResult: ", parseResult)
+    //     })
+    //     .catch(console.error);
 });
 module.exports = router;
